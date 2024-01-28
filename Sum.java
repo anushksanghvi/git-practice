777392cda56953baabbae2ada8489e64e4c50561
anushksanghvi <anushksanghvi@gmail.com>
2024-01-28 23:10:22 +0530

    Sum of two number added

---
+++ Sum.java
@@ -0,0 +1,10 @@
+public class Sum {
+
+    public static int SumNum(int a, int b) {
+        return (a+b);
+    }
+    public static void main(String[] args) {
+        
+        System.out.println("Sum = " + SumNum(45, 86));
+    }
+}
